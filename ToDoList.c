094f0410387ddf977319d79016ef9b099a25977e
aicha <aicha70tabite@gmail.com>
2023-09-20 14:37:26 +0100

    add my first function for add task

--- ToDoList.c
+++ ToDoList.c
@@ -1,87 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
-// Structure pour représenter une tache
+#define MAX_TASK 100
+
+// Structure to represent a task
 struct Task {
-    int id,deadline;
+    int id;
     char title[100];
-    char description[500];
+    char desc[500];
     char status[30];
+    int deadline;
 };
 
+int taskCount = 0;
 
-// Fonction pour ajouter une tache
-void addTask(struct Task* tasks[], int* taskCount) {
-  
-}
-
-// Fonction pour afficher la liste de toutes les taches
-void displayTasks(struct Task* tasks[], int taskCount) {
-}
-
-// Fonction pour modifier une tache
-void modifyTask(struct Task* tasks[], int taskCount) {
-}
-
-// Fonction pour supprimer une tache par identifiant
-void deleteTask(struct Task* tasks[], int* taskCount) {
-}
-
-// Fonction pour rechercher une tache par identifiant
-void searchTaskById(struct Task* tasks[], int taskCount) {
-}
-
-// Fonction pour rechercher une tache par titre
-void searchTaskByTitle(struct Task* tasks[], int taskCount) {
+// Function to add a new task
+void addTask(struct Task tasks[], const char *title, const char *desc, const char *status, int deadline) {
+    if (taskCount < MAX_TASK) {
+        struct Task newTask;
+        newTask.id = taskCount + 1; // Auto-incrementing ID
+        strncpy(newTask.title, title, sizeof(newTask.title));
+        strncpy(newTask.desc, desc, sizeof(newTask.desc));
+        strncpy(newTask.status, status, sizeof(newTask.status));
+        newTask.deadline = deadline;
+        tasks[taskCount] = newTask;
+        taskCount++;
+        printf("Task added successfully!\n");
+    } else {
+        printf("The task list is full. Cannot add more tasks.\n");
+    }
 }
 
 int main() {
-    struct Task tasks[100]; // Vous pouvez ajuster la taille du tableau en fonction de vos besoins
-    int taskCount = 0;
-    int choice;
+    struct Task tasks[MAX_TASK];
+    int choice, deadline;
+    char title[100], desc[500], status[30];
 
-    while (1) {
-        printf("==================================Bienvenue dans l'application de gestion de taches!==================================\n");
-        printf("=      1. Ajouter une nouvelle tache                                                                                 =\n");
-        printf("=      2. Ajouter plusieur tache                                                                                     =\n");
-        printf("=      2. Afficher la liste de toutes les taches                                                                     =\n");
-        printf("=      3. Modifier une tache                                                                                         =\n");
-        printf("=      4. Supprimer une tache                                                                                        =\n");
-        printf("=      5. Rechercher une tache par identifiant                                                                       =\n");
-        printf("=      6. Rechercher une tache par titre                                                                             =\n");
-        printf("=      2. Afficher les statiques                                                                                     =\n");
-        printf("=      7. Quitter                                                                                                    =\n");
-        printf("==================================================Entrez votre choix :================================================ \n");
+   do {
+        printf("==================================Welcome to the Task Management Application!==================================\n");
+        printf("=      1. Add a new task                                                                                      =\n");
+        printf("=      2. Add multiple tasks                                                                                  =\n");
+        printf("=      3. Display the list of all tasks                                                                       =\n");
+        printf("=      4. Modify a task                                                                                       =\n");
+        printf("=      5. Delete a task                                                                                       =\n");
+        printf("=      6. Search for a task by ID                                                                             =\n");
+        printf("=      7. Search for a task by title                                                                          =\n");
+        printf("=      8. Display statistics                                                                                  =\n");
+        printf("=      9. Quit                                                                                                =\n");
+        printf("==================================================Enter your choice:===========================================\n");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                addTask(tasks, &taskCount);
+                printf("Enter the title of the task: ");
+                scanf("%99s", title);
+                printf("Enter the description of the task: ");
+                scanf("%499s", desc);
+                printf("Enter the status of the task: ");
+                scanf("%29s", status);
+                printf("Enter the deadline: ");
+                scanf("%d", &deadline);
+                addTask(tasks, title, desc, status, deadline);
                 break;
             case 2:
-                displayTasks(tasks, taskCount);
+                // Add code to add multiple tasks here
                 break;
             case 3:
-                modifyTask(tasks, taskCount);
+                // Add code to display tasks here
                 break;
             case 4:
-                deleteTask(tasks, &taskCount);
+                // Add code to modify a task here
                 break;
             case 5:
-                searchTaskById(tasks, taskCount);
+                // Add code to delete a task here
                 break;
             case 6:
-                searchTaskByTitle(tasks, taskCount);
+                // Add code to search for a task by ID here
                 break;
             case 7:
-                printf("Au revoir!\n");
+                // Add code to search for a task by title here
+                break;
+            case 8:
+                // Add code to display statistics here
+                break;
+            case 9:
+                printf("Goodbye!\n");
                 exit(0);
             default:
-                printf("Choix invalide. Veuillez reessayer.\n");
+                printf("Invalid choice. Please try again.\n");
         }
-    }
-
+    
+  } while (choice != 9);
     return 0;
 }
